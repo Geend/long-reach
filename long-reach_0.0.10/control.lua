23e77cf09ce7728631947478dd8c5248f3ad4060
Torben Voltmer <git@t-voltmer.net>
2018-02-05 01:20:40 +0100

    Fixed multiplayer desyncing

--- long-reach_0.0.10/control.lua
+++ long-reach_0.0.10/control.lua
@@ -1,30 +1,23 @@
 script.on_init(function ()
-	sync_for_connected_players()	
+	apply_long_reach_settings()	
 end)
 
-script.on_configuration_changed(function ()
-	sync_for_connected_players()		
+script.on_configuration_changed(function (data)
+	apply_long_reach_settings()		
 end)
 
 script.on_event(defines.events.on_runtime_mod_setting_changed,function ()
-	sync_for_connected_players()
+	apply_long_reach_settings()
 end)
 
 
-function sync_for_connected_players()
-	for index,player in pairs(game.connected_players) do 
-		log(index)
-		sync_long_reach_settings(index)	
-	end
-end
-
-function sync_long_reach_settings(player_index)
+function apply_long_reach_settings()
 	local settings = settings.global
 	local default_action_distance = 6
 	
 	
-	game.players[player_index].character_build_distance_bonus = settings["long-reach-build-distance"]["value"] - default_action_distance
-	game.players[player_index].character_reach_distance_bonus = settings["long-reach-reach-distance"]["value"] - default_action_distance
+	game.forces["player"].character_build_distance_bonus = settings["long-reach-build-distance"]["value"] - default_action_distance
+	game.forces["player"].character_reach_distance_bonus = settings["long-reach-reach-distance"]["value"] - default_action_distance
 	
 end
 
